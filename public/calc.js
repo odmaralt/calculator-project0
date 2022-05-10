8e09bd524117099e6a8b59204c1b12caf5ccd25c
Odmaral Tumenjargal <odmaraltumenjargal@Odmarals-MacBook-Air.local>
2022-05-09 17:01:24 -0700

    reset JS

--- public/calc.js
+++ public/calc.js
@@ -1,100 +1,46 @@
-const number = document.querySelectorAll("#number");
-const operator = document.querySelectorAll("#operator");
-let previousAns = document.getElementById("previousAns");
-let currentAns = document.getElementById("currentAns");
-const ac = document.getElementById("ac");
-const ce = document.getElementById("ce");
-const equal = document.getElementById("equal");
-let isDot = false;
-let selectedOperation;
-ac.addEventListener("click", clear);
-number.forEach((n) => {
-  n.addEventListener("click", function () {
-    if (isDot === true && n.innerText === ".") {
-      return;
-    } else if (n.innerText === "." && isDot !== true) {
-      isDot = true;
-      appendNumberFunction(n.innerText);
-    } else if (n.innerText === "(-)") {
-      appendNumberFunction("-");
-    } else {
-      appendNumberFunction(n.innerText);
-    }
-  });
-});
-ce.addEventListener("click", function () {
-  let answer = currentAns.innerText;
-  currentAns.innerText = answer.slice(0, -1);
-});
-equal.addEventListener("click", function () {
-  compute();
-});
+// const number, operator (those two get queryselectorall),currentans, ac ,ce ,equal
+// let selectedoperation,and isdot be false
+// * all n, o, and currentAns are followed by innertext
+// *for if statements in parentheses, theres three equal signs
+// *compute and clear take empty parentheses
+// it goes ac, number, ce, equal, operator,f clear, f appendNumberFunction, f chooseOperation, f compute
+// Add event listeners:
+// ac -> click clear
+// equal-> click emptyfunction compute
+// ce-> click emptyfunction and inside function let answer be equal to currentans, currentans equals answer slice 0,-1
 
-operator.forEach((o) => {
-  o.addEventListener("click", function () {
-    if (
-      currentAns.innerText.includes("+") ||
-      currentAns.innerText.includes("-") ||
-      currentAns.innerText.includes("÷") ||
-      currentAns.innerText.includes("×")
-    ) {
-      compute();
-      selectedOperation = o.innerText;
-      appendNumberFunction(o.innerText);
-    } else {
-      isDot = false;
-      selectedOperation = o.innerText;
-      appendNumberFunction(o.innerText);
-    }
-  });
-});
-function clear() {
-  isDot = false;
-  previousAns.innerText = "";
-  currentAns.innerText = "";
-}
+// number for each takes n:
+// use => for the function
+// then do n AEL and do click then empty function, inside function:
+// if, is dot is true and n equal . just return
+// else if n has . and is dot is not true make isdot true and appendNumberFunction and inside that, n innertext
+// else if n has - appendNumberFunction "-"
+// else appendNumberFunction n innerText
 
-function appendNumberFunction(number) {
-  currentAns.innerText += number;
-}
+// do operator foreach and take o
+// o AEL empty function
+// if current ans includes + - * /: compute ;selectedOperation equals o innerText ;appendNumberFunction o innerText;
+// else, isDot is false; selectedOperation equals o innerText; appendNumberFunction o innerText;
 
-function chooseOperation(operator) {
-  selectedOperation = operator;
+// function clear takes nothing/empty parentheses : isDot is false; currentAns equals empty string;
 
-  if (currentAns.innerText === "") {
-    return;
-  }
+// function appendNumberFunction takes number and currentans += number
 
-  if (currentAns.innerText !== "") {
-    compute();
-  }
-}
+// function chooseOperation takes operator,
+// selectedoperation equals operator,
+// if currentans equals nothing, return
+// if currentans doesnt equal nothing, compute()
 
-function compute() {
-  if (selectedOperation === "+") {
-    currentAns.innerText = eval(currentAns.innerText);
-  } else if (selectedOperation === "-") {
-    currentAns.innerText = eval(currentAns.innerText);
-  } else if (selectedOperation === "×") {
-    console.log(currentAns.innerText);
-    let multiply = currentAns.innerText.split("×");
-    let total = 1;
-    for (let index = 0; index < multiply.length; index++) {
-      total *= Number(multiply[index]);
-    }
-    currentAns.innerText = total;
-  } else if (selectedOperation === "÷") {
-    let divide = currentAns.innerText.split("÷");
-    let firstDivider = 1;
-    let total1;
-    let answer;
-    for (let index = 0; index < divide.length; index++) {
-      if (index === 0) {
-        total1 = divide[index] / firstDivider;
-      } else {
-        answer = total1 /= divide[index];
-      }
-    }
-    currentAns.innerText = answer;
-  }
-}
+// function compute()
+// sign means selectedOperation
+// if sign is + or - use eval
+// else if sign is x , split currentAns with x and let it equal "multiply"
+// let the total be 1
+// and use for loop to do total times equal Number multiply outside index in brackets
+// after for loop currentans = total
+// else if sign is division sign:
+// split currentans with division sign and let it equal "divide"
+// let firstdivider equal 1 ,let total1 ,and let answer
+// then use forloop and if index is 0, meaning the first number being divided, make total1 equal divideindex divided by first firstDivider
+// else, answer is equal to the total1 of the first division divided by divideindex
+// outside forloop currentans equals answer
